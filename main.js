7c777d1e21c7142a2afd8310420f68da4b5a0d97
emillysobrinho <134522361+emillysobrinho@users.noreply.github.com>
2023-10-25 11:29:41 -0300

    up

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ function tocaSomClap(){
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 function tocaSomTim(){
-    document.querySelector('#som_tecla_Tim').play();
+    document.querySelector('#som_tecla_tim').play();
 }
-document.querySelector('.tecla_Tim').onclick = tocaSomTim;
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
