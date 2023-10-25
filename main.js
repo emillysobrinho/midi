558564be941d37c65b61d7f8808c246134893016
emillysobrinho <134522361+emillysobrinho@users.noreply.github.com>
2023-10-25 11:42:40 -0300

    up

--- main.js
+++ main.js
@@ -20,6 +20,11 @@ function tocaSomPuff(){
 }
 document.querySelector('.tecla_puff').onclick = tocaSomPuff;
 
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
 function tocaSomToim(){
     document.querySelector('#som_tecla_toim').play();
 }
