146d7c3a44f31f7115e0c558ff971288870038bc
emillysobrinho <134522361+emillysobrinho@users.noreply.github.com>
2023-10-25 11:44:26 -0300

    up

--- main.js
+++ main.js
@@ -34,3 +34,13 @@ function tocaSomPsh(){
     document.querySelector('#som_tecla_psh').play();
 }
 document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick = tocaSomTic;
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomTom;
